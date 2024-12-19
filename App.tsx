7bb43ea7f006c4ed0113f67d82d2cda04143c7bb
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-12-18 22:04:25 -0800

    FlashCards Added

--- App.tsx
+++ App.tsx
@@ -1,27 +1,17 @@
-import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import FlatCards from './compoents/FlatCards'
 
-function App() {
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.text}>Hello World!</Text>
-      </View>
+    <SafeAreaView>
+      <ScrollView>
+          <FlatCards>
+
+          </FlatCards>
+      </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center', // Vertically centers the text
-    alignItems: 'center', // Horizontally centers the text
-    backgroundColor: '#f0f0f0', // Light background for better visibility
-  },
-  text: {
-    fontSize: 24, // Make the text large enough to be visible
-    color: 'black', // Ensure the text is not the same color as the background
-  },
-});
-
-export default App;
+export default App
